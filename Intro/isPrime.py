e95f58a27e6a76e05819ddde2407b0c0871bf282
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-11-09 20:15:47 -0800

    solved is prime

--- Intro/isPrime.py
+++ Intro/isPrime.py
@@ -8,8 +8,14 @@
 
 
 def is_prime(n):
-
-    pass
+    if n < 2:
+        return False
+    
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+        
+    return True
 
 
 print(is_prime(2)) # -> True
